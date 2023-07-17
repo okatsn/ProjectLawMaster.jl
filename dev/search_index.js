37237862c8d8d50a8bd9cbb324ea605ce0b2386a
Documenter.jl <documenter@juliadocs.github.io>
2023-07-17 13:56:49 +0000

    build based on 4b3e7c0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProjectLawMaster","category":"page"},{"location":"#ProjectLawMaster","page":"Home","title":"ProjectLawMaster","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProjectLawMaster.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProjectLawMaster]","category":"page"}]
+}
