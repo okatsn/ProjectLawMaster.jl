e6318daee2c7419a90c666f46bec65bceaa6638c
okatsn <okatsn@gmail.com>
2023-07-25 15:51:42 +0800

    Some scripts

---
+++ scripts/summary_files.jl
@@ -0,0 +1,5 @@
+using OkFiles
+filelistall(r"+\.[a-zA-Z]+\Z", "temp")
+
+
+filelistall(raw"G:\.shortcut-targets-by-id\13nJtJKzw3hmGZIYqwBR0moSTjnXqGQ98\2023黑客松開放資料集")
---
+++ scripts/test_google_drive.jl
@@ -0,0 +1,3 @@
+
+using GoogleDrive
+sheet_handler("https://drive.google.com/drive/folders/13nJtJKzw3hmGZIYqwBR0moSTjnXqGQ98")
