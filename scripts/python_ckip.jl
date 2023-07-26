c5c07104a7aa34cb3f9707e236d2651bcc5f29cb
okatsn <okatsn@gmail.com>
2023-07-26 13:37:21 +0800

    CHECKPOINT: Trying CkipTagger

---
+++ scripts/python_ckip.jl
@@ -0,0 +1,18 @@
+using PyCall, Pkg
+Pkg.build("PyCall")
+
+# CHECKPOINT: please install in virtural machine. See https://github.com/ckiplab/ckiptagger#installation
+# `pip install -U ckiptagger` is necessary.
+
+# ckiptagger = pyimport("ckiptagger")
+
+
+# [CkipTagger](https://github.com/ckiplab/ckiptagger)
+# - Ckip Lab: https://ckip.iis.sinica.edu.tw/project/ws
+
+#
+
+
+# from ckiptagger import data_utils, construct_dictionary, WS, POS, NER
+
+# CHECKPOINT: Workable script: https://colab.research.google.com/drive/1H2BAdXB_OX7F0_-ePSWd8q8AdkJstEcR?fbclid=IwAR2jaV8fQ1WE3Ra02IkSFDAUrVbKIE436BW7fbDNw6Yi01AmsUgXYYco8eI#scrollTo=yYqa6VqXM0KK
